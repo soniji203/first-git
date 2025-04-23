9d6f8b390ca2b709f14fd9f7dae5e5bde86ca007
soniji203 <devsoniji203@gmail.com>
2025-04-23 12:21:08 +0530

    your message

---
+++ d.js
@@ -0,0 +1,9 @@
+//pangram;
+let s="thequickbrownfoxjumpsoverthelazydog";
+let set=new Set();
+for(let i=0;i<s.length;i++){
+    let ch=s.charAt(i);
+    set.add(ch);
+}
+if(set.size==26) console.log('is pangram');
+else console.log('not ');
